6cad2ae5ddfdfdcf2b51af92d937c8bb90fbc98e
p0ns <pons@p0ns.org>
2021-07-29 23:10:50 -0400

    Update mopidy.rb

--- lib/mopidy.rb
+++ lib/mopidy.rb
@@ -41,5 +41,6 @@ module Mopidy
   def self.post(body)
     headers = { 'Content-Type' => 'application/json' }
     res = Http.post(configuration.mopidy_url, body, headers)
+    res.has_error? ? res.body: nil
   end
 end
